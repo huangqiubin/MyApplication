baf350f5670d3a7a583e1b2c474a4b956bb0533e
huangqb <huangqb@weipaitang.com>
2019-11-28 10:17:04 +0800

    third commit

--- app/src/main/java/com/example/myapplication/java/Main.java
+++ app/src/main/java/com/example/myapplication/java/Main.java
@@ -6,10 +6,13 @@ import java.util.ArrayList;
 
 public class Main {
 
+
     public static void main(String[] args){
         ArrayList list = new ArrayList();
         list.add(new Integer(123));
         String first = list.get(0).toString();
+        System.out.println("这是我在dev1新增的修改！");
+
     }
 }
 
